db7373de9daba4051d5faa8f0d701cf4010957a9
Алексей Воронов <alexey.voronov96@gmail.com>
2018-09-19 02:32:42 +0500

    Initial Commit

---
+++ RSS Feed Reader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RSS Feed Reader
+//
+//  Created by Алексей Воронов on 19/09/2018.
+//  Copyright © 2018 Алексей Воронов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

62f56943814f21c23aba3e5f95b44608a31ebf81
AlexeyVoronov96 <43545084+AlexeyVoronov96@users.noreply.github.com>
2018-09-24 20:51:49 +0500

    Update NewsViewController.swift

--- RSS Feed Reader/NewsViewController.swift
+++ RSS Feed Reader/NewsViewController.swift
@@ -37,7 +37,13 @@ class NewsViewController: UIViewController, UITableViewDelegate, UITableViewData
         refreshControl.addTarget(self, action: #selector(refresh), for: .valueChanged)
         refreshControl.tintColor = UIColor.white
         tableView.addSubview(refreshControl)
-        fetchData()
+        
+        if isInternetAvailable() == true{
+            fetchData()
+        } else {
+            addSavedData()
+        }
+        
     }
     
     @objc func refresh(_ sender: Any) {
@@ -46,16 +52,19 @@ class NewsViewController: UIViewController, UITableViewDelegate, UITableViewData
     }
     
     public func fetchData() {
-        if isInternetAvailable() == true {
-            let feedParser = FeedParser()
-            feedParser.parseFeed(url: self.url!) { (rssItems) in
-                self.rssItems = rssItems
-            }
-        } else {
-            let data = UserDefaults.standard.value(forKey:self.url!) as? Data
-            let decodedData = try? PropertyListDecoder().decode(Array<RSSItem>.self, from: data!)
-            self.rssItems = decodedData
+        let feedParser = FeedParser()
+        feedParser.parseFeed(url: self.url!) { (rssItems) in
+            self.rssItems = rssItems
+        OperationQueue.main.addOperation {
+            self.tableView.reloadSections(IndexSet(integer: 0), with: .none)
         }
+        }
+    }
+    
+    func addSavedData(){
+        let data = UserDefaults.standard.value(forKey:self.url!) as? Data
+        let decodedData = try? PropertyListDecoder().decode(Array<RSSItem>.self, from: data!)
+        self.rssItems = decodedData
         OperationQueue.main.addOperation {
             self.tableView.reloadSections(IndexSet(integer: 0), with: .none)
         }
